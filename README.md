50985b9fdee728f119775c83fb6e9ba6deaa8220
Tim Benniks <tbenniks@gmail.com>
2022-10-06 20:22:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vue 3 turbo tutorials
+
+Browse this folder to find all Vue 3 Turbo Tutorials.
+Want to know more? Visit [https://turbo-tutorials.dev](https://turbo-tutorials.dev).
---
+++ vue3-how-to-make-a-composable/src/composables/useFriends.js
@@ -0,0 +1,17 @@
+import { ref } from "vue";
+
+const lines = [
+  {
+    name: "Rachel",
+    line: "...Maybe we should just take a break!",
+  },
+  {
+    name: "Ross",
+    line: "Let's cool off. Let's get some frozen yogurt or something.",
+  },
+];
+
+export function useFriends(who = "Ross") {
+  const line = ref(lines.find((actor) => actor.name === who));
+  return line;
+}
